f5be2b45988b626ed2ea5ca94920b5a452ea1f2a
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-11-05 23:39:10 +0600

    server side connect

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle were 
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.szoixno.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const productCollection = client.db('emaJohn').collection('products');
+
+
+        app.get('/products', async (req, res) => {
+
+            const page = req.query.page;
+            const size = req.query.size;
+            console.log(page, size);
+
+            const query = {}
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            const count = await productCollection.estimatedDocumentCount();
+
+            res.send({ count, products });
+        })
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send('ema john simple server is running');
+})
+
+
+app.listen(port, () => {
+    console.log(`This server is running on this ${port}`);
+})
